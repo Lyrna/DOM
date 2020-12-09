248ddd00b26dbd9ebc525188e032166f6ff4f1aa
Lyrna <clarisse.hurion@gmail.com>
2020-12-09 18:36:18 +0100

    fini

--- Exercice/script.js
+++ Exercice/script.js
@@ -1,6 +1,6 @@
 //récupérer le texte de la balise h1
 
-let titre = document.getElementsByTagName('h1')[0].innerHTML;
+let titre = document.getElementsByTagName('h1').textContent;
 console.log('Premier h1 -> Contenu : ' + titre);
 
 //on récupère les li
@@ -10,21 +10,28 @@ let liste = document.getElementsByTagName("li");
 let firstLI = liste[0].innerHTML;
 console.log('first LI : ' + firstLI);
 
+//premiere balise li
+let cible = document.querySelector("ul li:first-child").innerHTML;
+console.log('first LI2 : ' + cible);
+
 //dernière balise li
 let lastLI = liste[3].innerHTML;
 console.log('last LI : ' + lastLI);
+
+let dernier = document.querySelector("ul li:last-child").innerHTML;
+console.log('last LI2 : ' + dernier);
 
 //troisième balise li
 let thirdLI = liste[2].innerHTML;
 console.log('troisième LI : ' + thirdLI);
 
-//affichier le contenu html du parent de ul
+//afficher le contenu html du parent de ul
 
-let parent = document.querySelectorAll('#menu')[0].innerHTML;
+let parent = document.querySelector('#menu').innerHTML;
 console.log('HTML du parent de <ul> :' + parent);
 
 //afficher parent du quatrième li
-let parents = document.getElementsByTagName('ul')[0].innerHTML;
+let parents = document.querySelector('li:nth-child(4)').parentNode.innerHTML;
 console.log('HTML du parent du quatrième <li> :' + parents);
 
 //mouseover texte ul en rouge sinon noir
