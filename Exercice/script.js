3df07341733990f8d50bb92a047f4ee658f3d933
Lyrna <clarisse.hurion@gmail.com>
2020-12-09 16:15:47 +0100

    mouseover rouge fait

--- Exercice/script.js
+++ Exercice/script.js
@@ -23,5 +23,23 @@ console.log('troisième LI : ' + thirdLI);
 let parent = document.querySelectorAll('#menu')[0].innerHTML;
 console.log('HTML du parent de <ul> :' + parent);
 
+//afficher parent du quatrième li
 let parents = document.getElementsByTagName('ul')[0].innerHTML;
 console.log('HTML du parent du quatrième <li> :' + parents);
+
+//mouseover texte ul en rouge sinon noir
+//je cible l'id de la div qui contient l'ul
+//Addeventlistener pour le mouseover
+document.getElementById('menu').addEventListener("mouseover", mouseOver);
+//Addeventlistener pour le mouseout
+document.getElementById('menu').addEventListener("mouseout", mouseOut);
+
+//je colore en rouge pour le mouseover
+function mouseOver() {
+  document.getElementById('menu').style.color = "red";
+}
+
+//je recolore en noir si mouseout
+function mouseOut() {
+  document.getElementById('menu').style.color = "black";
+}
